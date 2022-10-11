6a36d57f389e5e776a5213c9302dead161a907ac
Glenn Winnink <gwinnink@student.codam.nl>
2022-10-11 13:15:57 +0200

    removed legacy code

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,21 +6,13 @@
 /*   By: gwinnink <gwinnink@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 15:20:46 by gwinnink          #+#    #+#             */
-/*   Updated: 2022/09/06 17:43:42 by gwinnink         ###   ########.fr       */
+/*   Updated: 2022/10/05 17:36:46 by gwinnink         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-typedef struct s_cmd
-{
-	char	*cmd;
-	char	**cmd_args;
-}	t_cmd;
-
 //----------------------------------------Prototypes
-t_cmd	cmd_parse(char *line, char **envp);
-int		count_split_cmd(char *line);
 
 #endif
