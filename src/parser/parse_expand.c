8d1bdb7b6a3075c7ab67989004108e27804ace55
Glenn Winnink <gwinnink@student.codam.nl>
2022-10-18 16:25:46 +0200

    created envvar expansion

---
+++ src/parser/parse_expand.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser_expand.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gwinnink <gwinnink@student.codam.nl>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/14 16:54:32 by gwinnink          #+#    #+#             */
+/*   Updated: 2022/10/14 16:55:00 by gwinnink         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "environment.h"
+#include "libft.h"
+
+char	*parser_expand(t_env *env, char *str)
+{
+	char	*ret;
+	char	*temp;
+	int		i;
+
+	i = 0;
+	while (str[i] && str[i] != ' ')
+		i++;
+	temp = ft_substr(str, 0, i);
+	ret = ft_strjoin(get_env(env, temp), &str[i]);
+	if (!ret)
+		ret = ft_strdup(&str[i]);
+	free(temp);
+	return (ret);
+}
