5123f2703b05f0c5b568ba121cceef4f1122c43f
Glenn Winnink <gwinnink@student.codam.nl>
2022-10-18 16:26:44 +0200

    moved funcitons to new files

--- src/parser/parser_utils.c
+++ src/parser/parser_utils.c
@@ -6,7 +6,7 @@
 /*   By: gwinnink <gwinnink@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/22 14:34:14 by gwinnink          #+#    #+#             */
-/*   Updated: 2022/10/13 17:17:17 by gwinnink         ###   ########.fr       */
+/*   Updated: 2022/10/18 13:51:09 by gwinnink         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,52 +27,5 @@ t_parser	*init_parser(void)
 		exit(-1);
 	ret->cmds = NULL;
 	ret->count = 0;
-	ret->limiter = NULL;
-	ret->in = NULL;
-	ret->out = NULL;
-	ret->append = 0;
-	return (ret);
-}
-
-char	*parser_expand(t_env *env, char *str)
-{
-	char	*ret;
-	char	*temp;
-	int		i;
-
-	i = 0;
-	while (str[i] && str[i] != ' ')
-		i++;
-	temp = ft_substr(str, 0, i);
-	ret = ft_strjoin(get_env(env, temp), &str[i]);
-	if (!ret)
-		ret = ft_strdup(&str[i]);
-	free(temp);
-	return (ret);
-}
-
-char	*append_token(t_env *env, char *str, t_token **token)
-{
-	char	*ret;
-	char	*temp;
-
-	if ((*token)->iden == EXPAND && (*token)->next->iden == DEF)
-	{
-			temp = parser_expand(env, (*token)->next->content);
-			(*token) = (*token)->next;
-	}
-	else if ((*token)->iden == EXPAND && !str)
-		temp = ft_strdup("");
-	else
-		temp = ft_strdup((*token)->content);
-	// printf("hi\n");
-	if (!str)
-		ret = ft_strdup(temp);
-	else
-	{
-		ret = ft_strjoin(str, temp);
-		free(str);
-	}
-	free(temp);
 	return (ret);
 }
